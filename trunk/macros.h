c1afa47a8ad0f348ea4450b8ea5795823a6e7704
m.akif.tutuncu@gmail.com <m.akif.tutuncu@gmail.com@17d46ad8-6b80-8c28-7918-56b049ed826f>
2011-06-06 10:51:47 +0000

    Macros

---
+++ trunk/macros.h
@@ -0,0 +1,24 @@
+/* BASIC STUDENT INFORMATION SYSTEM 1.0
+ * ================================================================================
+ * Pelin Öztürk & Mehmet Akif Tütüncü
+ * ================================================================================
+ * This project has been compiled with GNOME Compiler Collection under Ubuntu 11.04
+ * ================================================================================
+ * Declarations and Language Content
+ * ================================================================================
+ */
+
+// ===== Constants =====
+#define NAMELENGHT 73
+
+// ===== Version Information =====
+#define MAJORVERSION 1
+#define MINORVERSION 0
+
+// ===== English Language =====
+#define LANGUAGE_TITLE_MAINMENUBSIS             "BASIC STUDENT INFORMATION SYSTEM"
+#define LANGUAGE_TITLE_MAINMENU                 "MAIN MENU"
+#define LANGUAGE_TITLE_RETURNTOMAINMENU         "RETURNING TO THE MAIN MENU..."
+
+#define LANGUAGE_TITLE_CHOICE                   "Your choice"
+#define LANGUAGE_TITLE_INPUTERROR               "Please select one of the options available to you!"
