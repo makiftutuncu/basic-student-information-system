2a0ae206bff188a6b709c04affea3a08cf4bab09
m.akif.tutuncu@gmail.com <m.akif.tutuncu@gmail.com@17d46ad8-6b80-8c28-7918-56b049ed826f>
2011-06-14 11:28:35 +0000

    Some improvements and additions

--- trunk/macros.h
+++ trunk/macros.h
@@ -19,6 +19,46 @@
 #define LANGUAGE_TITLE_MAINMENUBSIS             "BASIC STUDENT INFORMATION SYSTEM"
 #define LANGUAGE_TITLE_MAINMENU                 "MAIN MENU"
 #define LANGUAGE_TITLE_RETURNTOMAINMENU         "RETURNING TO THE MAIN MENU..."
+#define LANGUAGE_TITLE_CREATEASTUDENT			"CREATE A STUDENT"
 
-#define LANGUAGE_TITLE_CHOICE                   "Your choice"
-#define LANGUAGE_TITLE_INPUTERROR               "Please select one of the options available to you!"
+#define LANGUAGE_MAINMENU_OPTION1               "Create a student"
+#define LANGUAGE_MAINMENU_OPTION2               "Exit"
+#define LANGUAGE_MAINMENU_CHOICE                "Your choice"
+
+#define LANGUAGE_INPUTERROR 		            "Please select one of the options available to you!"
+
+#define LANGUAGE_ANSWER_POSITIVEUPPER			'Y'
+#define LANGUAGE_ANSWER_POSITIVELOWER			'y'
+#define LANGUAGE_ANSWER_NEGATIVEUPPER			'N'
+#define LANGUAGE_ANSWER_NEGATIVELOWER			'n'
+
+#define LANGUAGE_CREATEASTUDENT_NAME			"Enter the name of the new student"
+#define LANGUAGE_CREATEASTUDENT_NUMBER			"Enter id number of the new student"
+#define LANGUAGE_CREATEASTUDENT_COURSE			"Do you want to add course information to this student? (Y/N)"
+#define LANGUAGE_CREATEASTUDENT_RESULT1			"New student"
+#define LANGUAGE_CREATEASTUDENT_RESULT2			"has been created and added to the AVL tree!"
+
+// ===== Turkish Language =====
+/*
+#define LANGUAGE_TITLE_MAINMENUBSIS             "TEMEL OGRENCI BILGI SISTEMI"
+#define LANGUAGE_TITLE_MAINMENU                 "ANA MENU"
+#define LANGUAGE_TITLE_RETURNTOMAINMENU         "ANA MENUYE DONULUYOR..."
+#define LANGUAGE_TITLE_CREATEASTUDENT			"OGRENCI OLUSTUR"
+
+#define LANGUAGE_MAINMENU_OPTION1               "Ogrenci olustur"
+#define LANGUAGE_MAINMENU_OPTION2               "Cikis"
+#define LANGUAGE_MAINMENU_CHOICE                "Seciminiz"
+
+#define LANGUAGE_INPUTERROR 		            "Lütfen size sunulan seceneklerden birini seciniz!"
+
+#define LANGUAGE_ANSWER_POSITIVEUPPER			'E'
+#define LANGUAGE_ANSWER_POSITIVELOWER			'e'
+#define LANGUAGE_ANSWER_NEGATIVEUPPER			'H'
+#define LANGUAGE_ANSWER_NEGATIVELOWER			'h'
+
+#define LANGUAGE_CREATEASTUDENT_NAME			"Yeni ogrencinin adini giriniz"
+#define LANGUAGE_CREATEASTUDENT_NUMBER			"Yeni ogrencinin numarasini giriniz"
+#define LANGUAGE_CREATEASTUDENT_COURSE			"Bu ogrenciye ders bilgisi eklemek istiyor musunuz? (E/H)"
+#define LANGUAGE_CREATEASTUDENT_RESULT1			"Yeni ogrenci"
+#define LANGUAGE_CREATEASTUDENT_RESULT2			"olusturuldu ve AVL agacina eklendi!"
+*/
